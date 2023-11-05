c06aa1628033cb816263db3ea53a45123a951c87
NW <nviet7532@gmail.com>
2023-11-05 15:32:31 +0700

    tạo layout và kết nối firebase, import 1 số thư viện cần thiết

--- app/src/main/java/com/example/pro1121_gr/activity/EditProfileActivity.java
+++ app/src/main/java/com/example/pro1121_gr/activity/EditProfileActivity.java
@@ -1,14 +1,16 @@
-package com.example.pro1121_gr;
+package com.example.pro1121_gr.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.pro1121_gr.R;
+
+public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_profile);
     }
 }
