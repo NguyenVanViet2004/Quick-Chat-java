74eb77f64b264b24b936f9430d38d25bd1e2e090
NW <nviet7532@gmail.com>
2023-11-06 16:13:06 +0700

    add model, function

--- app/src/main/java/com/example/pro1121_gr/model/chatRoomModel.java
+++ app/src/main/java/com/example/pro1121_gr/model/chatRoomModel.java
@@ -1,4 +1,64 @@
 package com.example.pro1121_gr.model;
 
+import com.google.firebase.Timestamp;
+
+import java.util.List;
+
 public class chatRoomModel {
+    String chatroomId;
+    List<String> userIds;
+    Timestamp lastMessageTimestamp;
+    String lastMessageSenderId;
+    String lastMessage;
+
+    public chatRoomModel() {
+    }
+
+    public chatRoomModel(String chatroomId, List<String> userIds, Timestamp lastMessageTimestamp, String lastMessageSenderId, String lastMessage) {
+        this.chatroomId = chatroomId;
+        this.userIds = userIds;
+        this.lastMessageTimestamp = lastMessageTimestamp;
+        this.lastMessageSenderId = lastMessageSenderId;
+        this.lastMessage = lastMessage;
+    }
+
+    public String getChatroomId() {
+        return chatroomId;
+    }
+
+    public void setChatroomId(String chatroomId) {
+        this.chatroomId = chatroomId;
+    }
+
+    public List<String> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(List<String> userIds) {
+        this.userIds = userIds;
+    }
+
+    public Timestamp getLastMessageTimestamp() {
+        return lastMessageTimestamp;
+    }
+
+    public void setLastMessageTimestamp(Timestamp lastMessageTimestamp) {
+        this.lastMessageTimestamp = lastMessageTimestamp;
+    }
+
+    public String getLastMessageSenderId() {
+        return lastMessageSenderId;
+    }
+
+    public void setLastMessageSenderId(String lastMessageSenderId) {
+        this.lastMessageSenderId = lastMessageSenderId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
 }
