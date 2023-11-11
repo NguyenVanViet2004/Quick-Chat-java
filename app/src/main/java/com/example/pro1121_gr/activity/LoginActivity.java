1d5bed1315767102bb9e84340f022bdb8c6962e4
NW <nviet7532@gmail.com>
2023-11-11 22:30:35 +0700

    fix Heisenbug

--- app/src/main/java/com/example/pro1121_gr/activity/LoginActivity.java
+++ app/src/main/java/com/example/pro1121_gr/activity/LoginActivity.java
@@ -3,20 +3,31 @@ package com.example.pro1121_gr.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.pro1121_gr.R;
 import com.example.pro1121_gr.databinding.ActivityLoginBinding;
+import com.example.pro1121_gr.util.NetworkChangeReceiver;
+
+import es.dmoral.toasty.Toasty;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private NetworkChangeReceiver networkChangeReceiver;
     private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Khởi tạo và đăng ký BroadcastReceiver
+        networkChangeReceiver = new NetworkChangeReceiver();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeReceiver, intentFilter);
 
         binding.countryCodePicker.registerCarrierNumberEditText(binding.edtLogin);
 
@@ -32,5 +43,14 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Hủy đăng ký BroadcastReceiver khi hoạt động bị hủy
+        super.onDestroy();
+        if (networkChangeReceiver != null) {
+            unregisterReceiver(networkChangeReceiver);
+        }
     }
 }
